f52c51b6558be47efbe724b7e88ce37fe4674030
sabahatsf <136477356+sabahatsf@users.noreply.github.com>
2024-02-21 08:37:58 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+function myFunction1() {
+  alert("your product has succesfully add to cart");
+}
+
+  
